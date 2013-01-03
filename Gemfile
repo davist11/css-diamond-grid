2e24a73c5b81820b2af179cec18031c872abbd3b
Trevor Davis <davist11@gmail.com>
2013-01-03 11:31:41 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source :rubygems
+
+gem "sass"
+gem "compass"
---
+++ config.rb
@@ -0,0 +1,9 @@
+#paths 
+http_path = "/"
+css_dir = "stylesheets/"
+sass_dir = "sass/"
+javascripts_dir = "scripts"
+
+# settings
+relative_assets = true
+line_comments = true
